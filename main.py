410c98ce32813f7190bc56289d86cbd1b4ccc448
Igor Freitas <igorpablo.sf@gmail.com>
2022-03-28 13:55:46 -0400

    Limpeza da estrutura

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from gui_v2 import Tela_main
+from gui_v3 import *
 
 root = Tela_main()
+frame_root = Frame_root(root._tela)
+menu = Menu_bar(root._tela, frame_root)
+botao = Variavel(frame_root._frame)
 root.Cria_tela()
+
